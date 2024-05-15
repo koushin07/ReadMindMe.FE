5480bd5fc4fea94f820366c612b426f4e1bcd0ac
miko <canaremiko3@gmail.com>
2024-05-15 23:45:07 +0800

    fixing endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-     apiUrl: 'https://read-me.azurewebsites.net//api',
-    hubUrl: 'https://read-me.azurewebsites.net//hubs'
+     apiUrl: 'https://read-me.azurewebsites.net/api',
+    hubUrl: 'https://read-me.azurewebsites.net/hubs'
 };
