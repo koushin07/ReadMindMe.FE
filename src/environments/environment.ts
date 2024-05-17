f6727f8799fc0c9dc62a1d11436e58873c026f30
miko <canaremiko3@gmail.com>
2024-05-17 23:28:14 +0800

    changes in endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-     apiUrl: 'http://localhost:5127/api',
-    hubUrl: 'http://localhost:5127/hubs'
+    apiUrl: 'https://read-me.azurewebsites.net/api',
+    hubUrl: 'https://read-me.azurewebsites.net/hubs'
 };
